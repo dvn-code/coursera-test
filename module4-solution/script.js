ba350a47d818a04b618be138b9c3275caf041d04
dvn-code <dvnellen@gmail.com>
2017-07-24 13:22:01 -0500

    Solution to Assignment 4

---
+++ module4-solution/script.js
@@ -0,0 +1,14 @@
+(function () {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+ 
+  for (var i in names) {
+
+    var firstLetter = names[i].charAt (0);
+
+    if (firstLetter.toLowerCase () == "j") {
+      byeSpeaker.speak (names[i])
+    } else {
+      helloSpeaker.speak (names[i])
+    }
+  }
+})();
